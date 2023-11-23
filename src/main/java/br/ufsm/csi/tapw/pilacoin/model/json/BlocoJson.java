1307a7a370f6eef9e3cffe054d8647cbac0f0249
nirewen <londero.edu@gmail.com>
2023-11-23 09:12:44 -0300

    fix: criação do bloco descoberto

--- src/main/java/br/ufsm/csi/tapw/pilacoin/model/json/BlocoJson.java
+++ src/main/java/br/ufsm/csi/tapw/pilacoin/model/json/BlocoJson.java
@@ -1,12 +1,16 @@
 package br.ufsm.csi.tapw.pilacoin.model.json;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BlocoJson {
     private String numeroBloco;
     private String nonceBlocoAnterior;
@@ -14,4 +18,5 @@ public class BlocoJson {
     private byte[] chaveUsuarioMinerador;
     private String nomeUsuarioMinerador;
     private List<TransacaoJson> transacoes;
+    private Boolean minerado;
 }
--- src/main/java/br/ufsm/csi/tapw/pilacoin/model/json/TransacaoJson.java
+++ src/main/java/br/ufsm/csi/tapw/pilacoin/model/json/TransacaoJson.java
@@ -1,12 +1,16 @@
 package br.ufsm.csi.tapw.pilacoin.model.json;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransacaoJson {
     private String id;
     private byte[] chaveUsuarioOrigem;
