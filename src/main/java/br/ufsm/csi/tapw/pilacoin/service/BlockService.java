b7b2a943ab4530d20f435b6af1112ba3450ad040
nirewen <londero.edu@gmail.com>
2023-11-27 21:29:57 -0300

    refactor: spawnar thread pra minerar bloco

--- src/main/java/br/ufsm/csi/tapw/pilacoin/service/BlockService.java
+++ src/main/java/br/ufsm/csi/tapw/pilacoin/service/BlockService.java
@@ -7,6 +7,7 @@ import br.ufsm.csi.tapw.pilacoin.util.CryptoUtil;
 import br.ufsm.csi.tapw.pilacoin.util.JacksonUtil;
 import br.ufsm.csi.tapw.pilacoin.util.Logger;
 import br.ufsm.csi.tapw.pilacoin.util.SharedUtil;
+import lombok.SneakyThrows;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -34,35 +35,16 @@ public class BlockService implements Observer<Difficulty> {
             return;
         }
 
-        blocoJson.setChaveUsuarioMinerador(this.sharedUtil.getPublicKey().getEncoded());
-        blocoJson.setNomeUsuarioMinerador(this.sharedUtil.getProperties().getUsername());
+        Thread t = new Thread(new BlockMinerRunnable(blocoJson, this.difficulty));
 
-        int count = 0;
+        t.setName("BlockMinerThread");
 
-        while (true) {
-            count++;
-
-            blocoJson.setNonce(CryptoUtil.getRandomNonce());
-
-            String minedJson = JacksonUtil.toString(blocoJson);
-
-            if (CryptoUtil.compareHash(minedJson, this.difficulty.getDificuldade())) {
-                this.queueService.publishBlocoMinerado(blocoJson);
-
-                break;
-            }
-        }
-
-        Logger.logBox(STR. """
-            BLOCO MINERADO
-            ---
-            Em \{ count } tentativas
-            """ );
+        t.start();
     }
 
     @RabbitListener(queues = "${queue.bloco.minerado}")
     public void validaBloco(@Payload String json) {
-        Logger.log(json);
+//        Logger.log(json);
         if (this.difficulty == null || json == null || json.isEmpty()) {
             return;
         }
@@ -71,5 +53,47 @@ public class BlockService implements Observer<Difficulty> {
     @Override
     public void update(Difficulty subject) {
         this.difficulty = subject;
+    }
+
+    public class BlockMinerRunnable implements Runnable {
+        private final Difficulty difficulty;
+        private final BlocoJson blocoJson;
+
+        public BlockMinerRunnable(BlocoJson blocoJson, Difficulty difficulty) {
+            this.difficulty = difficulty;
+            this.blocoJson = blocoJson;
+
+            blocoJson.setChaveUsuarioMinerador(sharedUtil.getPublicKey().getEncoded());
+            blocoJson.setNomeUsuarioMinerador(sharedUtil.getProperties().getUsername());
+        }
+
+        @Override
+        @SneakyThrows
+        public void run() {
+            int count = 0;
+
+            Logger.logBox("Minerando bloco...");
+
+            while (true) {
+                count++;
+
+                blocoJson.setNonce(CryptoUtil.getRandomNonce());
+
+                String minedJson = JacksonUtil.toString(blocoJson);
+
+                if (CryptoUtil.compareHash(minedJson, this.difficulty.getDificuldade())) {
+                    queueService.publishBlocoMinerado(blocoJson);
+
+                    break;
+                }
+            }
+
+            Logger.logBox(STR. """
+                BLOCO MINERADO
+                ---
+                Em \{ count } tentativas
+                """ );
+            Thread.currentThread().interrupt();
+        }
     }
 }
