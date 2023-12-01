cab99d1fda4860ae6f6665afef8e66e94e570c7e
nirewen <londero.edu@gmail.com>
2023-11-30 22:52:59 -0300

    feat: filtragem na query

--- src/main/java/br/ufsm/csi/tapw/pilacoin/controller/QueryController.java
+++ src/main/java/br/ufsm/csi/tapw/pilacoin/controller/QueryController.java
@@ -1,53 +1,72 @@
 package br.ufsm.csi.tapw.pilacoin.controller;
 
 import br.ufsm.csi.tapw.pilacoin.model.json.QueryJson;
+import br.ufsm.csi.tapw.pilacoin.model.json.QueryResponseJson;
 import br.ufsm.csi.tapw.pilacoin.service.QueueService;
+import br.ufsm.csi.tapw.pilacoin.util.SharedUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/query")
 public class QueryController {
-    private QueueService queueService;
+    private final QueueService queueService;
+    private final SharedUtil sharedUtil;
 
-    public QueryController(QueueService queueService) {
+    public QueryController(QueueService queueService, SharedUtil sharedUtil) {
         this.queueService = queueService;
+        this.sharedUtil = sharedUtil;
     }
 
     @GetMapping(value = "/usuarios", produces = "application/json")
-    public Object getUsuarios() {
-        return this.queueService.requestQuery(
-            QueryJson
-                .builder()
-                .idQuery(1L)
-                .nomeUsuario("londeroedu")
-                .tipoQuery(QueryJson.TipoQuery.USUARIOS)
-                .build()
-        );
+    public QueryResponseJson getUsuarios() {
+        QueryJson query = QueryJson
+            .builder()
+            .idQuery(1L)
+            .nomeUsuario(this.sharedUtil.getProperties().getUsername())
+            .tipoQuery(QueryJson.TipoQuery.USUARIOS)
+            .build();
+
+        return this.queueService.requestQuery(query);
     }
 
     @GetMapping(value = "/pilas", produces = "application/json")
-    public Object getPilas() {
-        return this.queueService.requestQuery(
-            QueryJson
-                .builder()
-                .idQuery(2L)
-                .nomeUsuario("londeroedu")
-                .tipoQuery(QueryJson.TipoQuery.PILA)
-                .build()
-        );
+    public QueryResponseJson getPilas(@RequestParam Map<String, String> filter) {
+        QueryJson query = QueryJson
+            .builder()
+            .idQuery(2L)
+            .nomeUsuario(this.sharedUtil.getProperties().getUsername())
+            .tipoQuery(QueryJson.TipoQuery.PILA)
+            .build();
+
+        if (filter.get("self") != null && filter.get("self").equals("true")) {
+            query.setUsuarioMinerador(this.sharedUtil.getProperties().getUsername());
+        } else {
+            query.setUsuarioMinerador(filter.get("usuarioMinerador"));
+        }
+
+        return this.queueService.requestQuery(query);
     }
 
     @GetMapping(value = "/blocos", produces = "application/json")
-    public Object getBlocos() {
-        return this.queueService.requestQuery(
-            QueryJson
-                .builder()
-                .idQuery(3L)
-                .nomeUsuario("londeroedu")
-                .tipoQuery(QueryJson.TipoQuery.BLOCO)
-                .build()
-        );
+    public QueryResponseJson getBlocos(@RequestParam Map<String, String> filter) {
+        QueryJson query = QueryJson
+            .builder()
+            .idQuery(3L)
+            .nomeUsuario(this.sharedUtil.getProperties().getUsername())
+            .tipoQuery(QueryJson.TipoQuery.BLOCO)
+            .build();
+        
+        if (filter.get("self") != null && filter.get("self").equals("true")) {
+            query.setUsuarioMinerador(this.sharedUtil.getProperties().getUsername());
+        } else {
+            query.setUsuarioMinerador(filter.get("usuarioMinerador"));
+        }
+
+        return this.queueService.requestQuery(query);
     }
 }
